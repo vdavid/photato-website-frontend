0b87383c2389e33b938650d8238b961b9b6090cc
David Veszelovszki <veszelovszki@gmail.com>
2020-06-07 15:26:12 +0200

    Added stubs and metadata for own articles.

---
+++ src/materials/own-content/hu/week7-long-exposure.mjs
@@ -0,0 +1,26 @@
+import React from '../../../web_modules/react.js';
+
+// noinspection JSUnusedGlobalSymbols (This file is loaded dynamically.)
+/**
+ * @returns {ArticleMetadata}
+ */
+export function getMetadata() {
+    // noinspection SpellCheckingInspection (It's in Hungarian.)
+    return {
+        slug: 'hosszu-zarido-tippek',
+        title: 'Tippek hosszú záridős fotózáshoz',
+        author: 'Photato',
+        publishDate: new Date('2020-06-07'),
+        publisherName: 'Photato',
+    };
+}
+
+// noinspection JSUnusedGlobalSymbols (This file is loaded dynamically.)
+/**
+ * @returns {React.ReactElement}
+ */
+export default function Article() {
+    // noinspection SpellCheckingInspection (It's in Hungarian.)
+    return <>
+    </>;
+}
