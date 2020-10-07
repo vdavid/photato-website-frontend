977c99677834b45697e5af16a4b5f4b3f297755e
David Veszelovszki <veszelovszki@gmail.com>
2020-10-07 10:45:31 +0200

    Date fix

--- src/config.mjs
+++ src/config.mjs
@@ -4,7 +4,7 @@ const cloudFrontBackEndUrl = 'https://dglg96wn4of1.cloudfront.net';
 /* Course settings */
 const startYear = 2020;
 const startMonth = 11;
-const startDay = 8; /* Must be the Sunday morning 0:00 CET when the course starts */
+const startDay = 9; /* Must be the Monday when the course starts */
 const isDaylightSavingTimeOn = true; /* Usually from the end of March till the end of October, but different every year */
 const isWinterOrSummerCourse = 'winter';
 
