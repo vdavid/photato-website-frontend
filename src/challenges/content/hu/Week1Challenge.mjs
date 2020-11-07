00793cc2177bf31ca2fada32ce9feddce4af504f
David Veszelovszki <veszelovszki@gmail.com>
2020-11-07 21:35:45 +0100

    "Gastrophoto" meaning clarification

--- src/challenges/content/hu/Week1Challenge.mjs
+++ src/challenges/content/hu/Week1Challenge.mjs
@@ -12,7 +12,7 @@ export default function Week1Challenge({formattedDeadline}) {
         <p>Az első hét témája: <strong>gasztrofotó</strong>!
         </p>
         <FullWidthLocalImage fileName="pizza.jpg" altText="pizza"/>
-        <p>A gasztrofotózást tökéletes első témának tartjuk, mert kevés lelkesítőbb fotós kihívást ismerünk, mint finom, színes kajákat fényképezni 😋, és mert a legtöbben azért még bőven tanulhatunk arról, hogy hogyan lehet ezt igazán profin csinálni.</p>
+        <p>Az ételek fotózását – más néven “gasztrofotózást” – tökéletes első témának tartjuk, mert kevés lelkesítőbb fotós kihívást ismerünk, mint finom, színes kajákat fényképezni 😋, és mert a legtöbben azért még bőven tanulhatunk arról, hogy hogyan lehet ezt igazán profin csinálni.</p>
 
         <p>
             <strong>Az első heti feladatod</strong> tehát ételeket/italokat fotózni, kiválasztani közülük a legjobbat, és ${formattedDeadline}-ig feltölteni <PhotoUploadLink label="ezen a linken"/>. A beazonosításhoz fontos, hogy a kép neve az email címed legyen: pl. “krumplipuree12@gmail.com.jpg”.
